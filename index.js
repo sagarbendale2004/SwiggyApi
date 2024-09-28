c2ef9a1515abbbdd62b4cbd794dcc9fbd5f78f2a
Sagar Bendale <99851764+sagarbendale2004@users.noreply.github.com>
2024-09-28 11:58:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = 8000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
